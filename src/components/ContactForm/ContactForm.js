363c845e272feb984ee39c056080f2d2ab06568d
openplusJK <jesse.kirschner@openplus.nl>
2020-01-15 15:26:16 +0100

    updating form to follow netlify example

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,30 +1,75 @@
 // @flow strict
 import React from 'react';
+import { navigate } from 'gatsby-link';
 import styles from './ContactForm.module.scss';
 
 type Props = {
   options: Object
 };
 
-const ContactForm = ({ options }: Props) => (
-  <div className={styles['contactForm']}>
-    <form method="post" action="#">
-      <label>
-        {options.name}
-        <input type="text" name="name" id="name" required/>
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" id="email" required/>
-      </label>
-      <label>
-        Message
-        <textarea name="message" id="message" rows="5" required/>
-      </label>
-      <button type="submit">Send</button>
-      <input type="reset" value="Clear" />
-    </form>
-  </div>
-);
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
+const ContactForm = ({ options }: Props) => {
+  const [state, setState] = React.useState({});
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div className={styles['contactForm']}>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}>
+
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+
+        <label>
+          {options.name}
+          <input type="text" name="name" id="name" required />
+        </label>
+        <label>
+          Email
+        <input type="email" name="email" id="email" required />
+        </label>
+        <label>
+          Message
+        <textarea name="message" id="message" rows="5" required />
+        </label>
+        <button type="submit">Send</button>
+        <input type="reset" value="Clear" />
+      </form>
+    </div>
+  );
+};
 
 export default ContactForm;
