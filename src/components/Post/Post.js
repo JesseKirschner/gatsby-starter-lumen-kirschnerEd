7f07530b3774646d0d58931a43d9261a047c59c6
openplusJK <jesse.kirschner@openplus.nl>
2020-03-02 21:41:40 +0100

    changed name AGAIN

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -20,7 +20,7 @@ const Post = ({ post }: Props) => {
 
   return (
     <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/blog">All Blog Posts</Link>
+      <Link className={styles['post__home-button']} to="/blog">&lt; Blogs</Link>
 
       <div className={styles['post__content']}>
         <Content body={html} title={title} />
